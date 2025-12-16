4b0eeaea5102303450ccb556ea308bd4799e7b2a
aminhaiqal <aminhaiqal15@gmail.com>
2025-12-17 00:38:00 +0800

    api to read user based on id

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from app.db.base import get_db
 from app.repositories.user_repo import UserRepo
-from app.schemas.user import CreateUserRequest
+from app.schemas.user import CreateUserRequest, ResponseUserRequest
 from app.services.user_service import UserService
 
 router = APIRouter(prefix="/user", tags=["users"])
@@ -20,3 +20,16 @@ def create_user(payload: CreateUserRequest, db: Session = Depends(get_db)):
     
     except SQLAlchemyError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    
+
+@router.get("/{user_id}", response_model=ResponseUserRequest)
+def get_user_by_id(user_id: str, db: Session = Depends(get_db)):
+    repo = UserRepo(db=db)
+    service = UserService(repo=repo, db=db)
+
+    user = service.get_by_id(user_id=user_id)
+    if user is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found")
+        
+    return user
+    
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,6 +1,18 @@
 from pydantic import BaseModel
+from decimal import Decimal
 
 class CreateUserRequest(BaseModel):
     email: str
     full_name: str
     currency: str
+
+class ResponseUserRequest(BaseModel):
+    user_id: str
+    email: str
+    full_name: str
+    wallet_balance: Decimal
+    currency: str
+    status: str
+
+    class config:
+        from_attribute = True
